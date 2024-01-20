128de34e3ec8217bdf933063ee7303aebc9b7bcb
tdenizgez <tdenizgez@gmail.com>
2024-01-20 12:39:56 +0300

    Initialize app

---
+++ src/main/java/com/personal/mertdenizgez/MertdenizgezApplication.java
@@ -0,0 +1,13 @@
+package com.personal.mertdenizgez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MertdenizgezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MertdenizgezApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/mertdenizgez/MertdenizgezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.mertdenizgez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MertdenizgezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
